8474d1c6fc9fba13bbab8ac04b22bba424ceb0eb
Francis Matias <fwmatias64@gmail.com>
2024-09-04 07:40:17 +0100

    mobile pop-up done

--- script/main.js
+++ script/main.js
@@ -1,7 +1,16 @@
+let popupMobile = document.getElementById("myPopup-mobile");
+let popup = document.getElementById("popup");
 // When the user clicks on div, open the popup
 function sharePopup() {
-    var popup = document.getElementById("popup");
     popup.classList.toggle("show");
+    popupMobile.classList.toggle("footer-show");
+    console.log('Popup visibility toggled');
+}
+
+console.log('JavaScript loaded');
+
+function sharePopupMobile() {
+    popupMobile.classList.toggle("footer-show");
     console.log('Popup visibility toggled');
 }
 
